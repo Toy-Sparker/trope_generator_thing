df266cbec18b51dd66765a9ea082efc17a8df9b6
Toy-Sparker <Rainbowsparkle40@gmail.com>
2023-01-01 16:06:41 +1300

    Commit #4 (01/01/2023) - More Tropes
    - Added more tropes
    - Also enabled stretching on the window size

--- Scripts/TropeLibrary.gd
+++ Scripts/TropeLibrary.gd
@@ -8,7 +8,7 @@ enum tropeTypes {
 
 var TropeDict : Dictionary = {
 	"Hot-Blooded": "Passionate about everything they do.",
-	"Woobie": "Suffers a lot and needs a hug.",
+	"The Woobie": "Suffers a lot and needs a hug.",
 	"Deadpan Snarker": "Drily snarks regularly.",
 	"The Chosen One": "The one who is chosen to save the world according to a chooser or to a prophercy.",
 	"Apocalypse Maiden": "A person is chosen to bring end the world.",
@@ -19,5 +19,12 @@ var TropeDict : Dictionary = {
 	"Stepford Smiler": "Happy on the outside. Depressed/empty/unstable on the inside.",
 	"Stepford Snarker": "Uses snark to hide negative emotions and/or hurt on the inside.",
 	"Shrinking Violet": "An insecure and very shy person.",
-	"Tranquil Fury": "Calm, but serious and violently angry."
+	"Tranquil Fury": "Calm, but serious and violently angry.",
+	"Anti-Nihilist": "Believes that life may be meaningless, but we can give it our own meaning.",
+	"One Man Army": "One who has the strength of many.",
+	"Ax-Crazy": "Dangerously unhinged, unstable, and prone to random violence.",
+	"The Eeyore": "Perpetually depressed and pessimistic character.",
+	"Despair Event Horizon": "Point of losing all hope.",
+	"Byronic Hero": "A troubled, brooding, sometimes selfish outcast whose passions and strive towards a goal cause drama.",
+	"Sanity Slippage": "Becomes increasingly insane throughout the story."
 }
--- trope_gen_test.service.worker.js
+++ trope_gen_test.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1672445756|186989305";
+const CACHE_VERSION = "1672542209|6163849515";
 const CACHE_PREFIX = "Trope Generator -sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "trope_gen_test.offline.html";
