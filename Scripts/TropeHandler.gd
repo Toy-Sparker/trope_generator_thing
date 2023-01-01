70d2e5892904cf5b7fa01def2410e23b39d8b8f5
Toy-Sparker <Rainbowsparkle40@gmail.com>
2023-01-01 17:17:08 +1300

    Commit #5 (01/01/2023) - Fixed squashed and can't read problem
    - On a phone, it was near impossible to read anything
    - Added a spin box so that you can scroll through the tropes available

--- Scripts/TropeHandler.gd
+++ Scripts/TropeHandler.gd
@@ -2,25 +2,36 @@ extends Node2D
 
 onready var TropeLabel = $UI/BasePanel/TropeTitlePanel/TropeLabel
 onready var TropeDescLabel = $UI/BasePanel/TropeDescPanel/TropeDescLabel
+onready var RollButton = $UI/BasePanel/Button
+onready var TropeBox = $UI/BasePanel/SpinBox
+var tropeDict = TropeLibrary.TropeDict
 var tropeNum = 0
 var randNumGen = RandomNumberGenerator.new()
 
 func _ready():
+	print(tropeDict.size())
+	TropeBox.max_value = tropeDict.size()-1
+	
 	roll_trope()
-
-func _process(delta):
-	pass
 
 func _on_Button_pressed():
 	roll_trope()
 
 func roll_trope():
-	var getDict = TropeLibrary.TropeDict
-	var getNumber = randNumGen.randi_range(0, getDict.size()-1)
+	var getNumber = randNumGen.randi_range(0, tropeDict.size()-1)
 	
 	while(getNumber == tropeNum):
-		getNumber = randNumGen.randi_range(0, getDict.size()-1)
+		getNumber = randNumGen.randi_range(0, tropeDict.size()-1)
 	
 	tropeNum = getNumber
-	TropeLabel.text = getDict.keys()[tropeNum]
-	TropeDescLabel.text = getDict.values()[tropeNum]
+	TropeBox.value = tropeNum
+	
+	# Commented out because I realized that the because the value of
+	# -- the trope box is being changed, it triggers the signal anyway.
+	#TropeLabel.text = tropeDict.keys()[tropeNum]
+	#TropeDescLabel.text = tropeDict.values()[tropeNum]
+
+func _on_SpinBox_value_changed(value):
+	tropeNum = value
+	TropeLabel.text = tropeDict.keys()[tropeNum]
+	TropeDescLabel.text = tropeDict.values()[tropeNum]
--- Scripts/TropeLibrary.gd
+++ Scripts/TropeLibrary.gd
@@ -7,24 +7,62 @@ enum tropeTypes {
 }
 
 var TropeDict : Dictionary = {
-	"Hot-Blooded": "Passionate about everything they do.",
-	"The Woobie": "Suffers a lot and needs a hug.",
-	"Deadpan Snarker": "Drily snarks regularly.",
-	"The Chosen One": "The one who is chosen to save the world according to a chooser or to a prophercy.",
-	"Apocalypse Maiden": "A person is chosen to bring end the world.",
-	"Anti-Hero": "A hero who does unheroic things and/or has a unheroic personality.",
-	"The Antichrist": "The chosen one but evil.",
-	"Knight in Sour Armor": "A cynical, bitter, and/or pessimistic character who still does good anyway.",
-	"The Snark Knight": "A world-weary character finds everyone worth snarking at, themselves included.",
-	"Stepford Smiler": "Happy on the outside. Depressed/empty/unstable on the inside.",
-	"Stepford Snarker": "Uses snark to hide negative emotions and/or hurt on the inside.",
-	"Shrinking Violet": "An insecure and very shy person.",
-	"Tranquil Fury": "Calm, but serious and violently angry.",
-	"Anti-Nihilist": "Believes that life may be meaningless, but we can give it our own meaning.",
-	"One Man Army": "One who has the strength of many.",
-	"Ax-Crazy": "Dangerously unhinged, unstable, and prone to random violence.",
-	"The Eeyore": "Perpetually depressed and pessimistic character.",
-	"Despair Event Horizon": "Point of losing all hope.",
-	"Byronic Hero": "A troubled, brooding, sometimes selfish outcast whose passions and strive towards a goal cause drama.",
+	"Hot-Blooded": 
+		"Passionate about everything they do.",
+	
+	"The Woobie": 
+		"Suffers a lot and needs a hug.",
+	
+	"Deadpan Snarker": 
+		"Drily snarks regularly.",
+	
+	"The Chosen One": 
+		"The one who is chosen to save the world according to a chooser or to a prophercy.",
+	
+	"Apocalypse Maiden": 
+		"A person is chosen to bring end the world.",
+	
+	"Anti-Hero": 
+		"A hero who does unheroic things and/or has a unheroic personality.",
+	
+	"The Antichrist": 
+		"The chosen one but evil.",
+	
+	"Knight in Sour Armor": 
+		"A cynical, bitter, and/or pessimistic character who still does good anyway.",
+	
+	"The Snark Knight": 
+		"A world-weary character finds everyone worth snarking at, themselves included.",
+	
+	"Stepford Smiler": 
+		"Happy on the outside. Depressed/empty/unstable on the inside.",
+	
+	"Stepford Snarker": 
+		"Uses snark to hide negative emotions and/or hurt on the inside.",
+	
+	"Shrinking Violet": 
+		"An insecure and very shy person.",
+	
+	"Tranquil Fury": 
+		"Calm, but serious and violently angry.",
+	
+	"Anti-Nihilist": 
+		"Believes that life may be meaningless, but we can give it our own meaning.",
+	
+	"One Man Army": 
+		"One who has the strength of many.",
+	
+	"Ax-Crazy": 
+		"Dangerously unhinged, unstable, and prone to random violence.",
+	
+	"The Eeyore": 
+		"Perpetually depressed and pessimistic character.",
+	
+	"Despair Event Horizon": 
+		"Point of losing all hope.",
+	
+	"Byronic Hero": 
+		"A troubled, brooding, sometimes selfish outcast whose passions and strive towards a goal cause drama.",
+	
 	"Sanity Slippage": "Becomes increasingly insane throughout the story."
 }
